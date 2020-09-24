4af96f36fe733aa511d7bcc2dc8386fe8fa21c37
valentiniym <71429395+valentiniym@users.noreply.github.com>
2020-09-24 18:57:39 +0200

    chore(fonction): new exercises fonction

---
+++ fonction/index.js
@@ -0,0 +1,13 @@
+"use strict"; 
+
+// Exercice 1 
+
+var myPutStr = fonction myPutStr (value) {
+if (typeof value === "string") {
+  return "its ok" ;
+}
+if (typeof value === "number") {
+  return "et pourquoi pas 42";
+}
+}
+myPutStr(15)
