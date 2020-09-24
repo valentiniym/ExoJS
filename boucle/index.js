da9119e9da8ebcbb51ae440544c5a3e8bdd66a61
valentiniym <71429395+valentiniym@users.noreply.github.com>
2020-09-24 13:06:15 +0200

    chore(boucle): boucle exercises

---
+++ boucle/index.js
@@ -0,0 +1,30 @@
+"use strict"; 
+
+// Exercice 1 
+
+var toMutiply = [1,2,3,5,8];
+   for(var key in toMutiply){
+       for(var i = 0; i<=10; i++){
+           var result = toMutiply[key] * i;
+            console.log(result);
+       }
+
+   }
+
+
+// Exercice 2 
+
+var toMutiply2 = [5];
+   for(var key in toMutiply2){
+       for(var i = 0; i<=10; i++){
+           var result = toMutiply2[key] * i;
+            console.log(i, "x", toMutiply2,"=", result);
+
+  var body = document.querySelector(body);
+  Element.querySelector(li)
+  <ul> 
+    <li>console.log(i, "x", toMutiply2,"=", result);</li>
+  </ul>
+       }
+
+   }
