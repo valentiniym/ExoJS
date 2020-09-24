de0edff92000c162418b9595cccb5e8440d984d0
valentiniym <71429395+valentiniym@users.noreply.github.com>
2020-09-24 19:28:42 +0200

    fix(fonction): exercises two and little about three

--- fonction/index.js
+++ fonction/index.js
@@ -2,12 +2,34 @@
 
 // Exercice 1 
 
-var myPutStr = fonction myPutStr (value) {
-if (typeof value === "string") {
-  return "its ok" ;
+var myPutStr = function myPutStr(value){
+  if (typeof value === "string") {
+    return "its ok" ;
+  }
+  if (typeof value === "number") {
+    return "et pourquoi pas 42";
+  }
+  }
+
+  myPutStr(15)
+  
+  // Exercice 2 
+
+  var computeSurfaceM2 = function computeSurfaceM2 (largeur, hauteur) {
+    return (largeur*hauteur + " m2 ")
+  }
+  
+  computeSurfaceM2(2,2)
+
+  // Exercice 3 
+
+var detectMyAgeByNight = function detectMyAgeByNight ( age ) {
+  if (age<=17) { 
+      alert ("Vous ne pouvez pas entrer vous n’êtes pas majeur vous avez " + age + " ans"); 
+    } else {
+      alert ("Vous pouvez entrer vous êtes majeur vous avez " + age + " ans") ; 
+  };
+  detectMyAgeByNight = prompt 
 }
-if (typeof value === "number") {
-  return "et pourquoi pas 42";
-}
-}
-myPutStr(15)
+
+detectMyAgeByNight(13)
